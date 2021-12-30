488c96139b008fc30e125d8d3ef2fea353119225
Francesco Palma <fp484839@gmail.com>
2021-12-30 19:30:09 +0100

    Aggiungere i file di progetto.

---
+++ ModelsLibrary/Author.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ModelsLibrary
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ ModelsLibrary/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelsLibrary
+{
+    class Book
+    {
+        public int ISBN { get; set; }
+        public string Title { get; set; }
+        public int PublicationYear { get; set; }
+        public int NumOfPages { get; set; }
+        public string Genre { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
